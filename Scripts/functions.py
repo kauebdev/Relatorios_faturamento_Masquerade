a0c146052d9e96aa3607c04b7efd0736f3a4a5ed
kauebdev <kaueoliveira190409@gmail.com>
2025-07-10 17:18:27 -0300

    feat: gerall

---
+++ Scripts/functions.py
@@ -0,0 +1,36 @@
+from datetime import date
+
+# -- pega o ano a ser utilizado
+def GetAno(): 
+    try:
+        ano = int(input("Digite o ano: ").strip())
+        return ano 
+    except ValueError:
+        print("Ano inválido. Por favor, insira um número inteiro.")
+    return None
+
+def GetMes():
+    try:
+        mes = int(input("Digite o mes: ").strip())
+        return mes 
+    except ValueError:
+        print("mes inválido. Por favor, insira um número inteiro.")
+    return None
+
+def GetPeriodo():
+    ano = GetAno()        
+    mes = GetMes()
+    try:
+        dia = int(input("Digite o dia: ").strip())
+        return date(ano,mes,dia)
+    except ValueError:
+        print("periodo inválido. Por favor, insira um número inteiro.")
+    return None
+
+def PeriodoMenu():
+        print("""escolha
+              1 - por ano
+              2 - por mes 
+              3 - de um mes/ano ate um mes/ ano
+              """)
+        opcao_periodo = int(input("digite o periodo que deja flitrar este relatorio: ").strip())
