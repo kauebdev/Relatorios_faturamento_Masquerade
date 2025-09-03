493f13a84dbf4ddc2758f19a47e355da0e10a317
kauebdev <kaueoliveira190409@gmail.com>
2025-09-03 15:47:54 -0300

    comi5

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -1,4 +1,7 @@
 from datetime import date
+import pandas as pd
+import os 
+import sys
 
 # -- pega o ano a ser utilizado
 def GetAno(): 
@@ -36,3 +39,36 @@ def PeriodoMenu():
         opcao_periodo = int(input("digite o periodo que deja flitrar este relatorio: ").strip())
 
 
+def EhNovo(mes):
+    base_dir =  r'C:\Users\ADMIN\Desktop\kaue\py\faturamento e bi mes a mes\Data'
+    
+    dfs = []
+    
+    # Percorrer os anos (BRUTOS-2024, BRUTOS-2025)
+    for ano in ["BRUTOS-2024", "BRUTOS-2025"]:
+        caminho_ano = os.path.join(base_dir, ano)
+        
+        if not os.path.exists(caminho_ano):
+            continue  # pula se a pasta do ano não existir
+        
+        # Percorrer as subpastas dentro do ano (ex.: 01-2024, 02-2024, ...)
+        for pasta_mes in os.listdir(caminho_ano):
+            caminho_arquivo = os.path.join(caminho_ano, pasta_mes, "sh-nfs_final.xlsx")
+            
+            if os.path.exists(caminho_arquivo):
+                df_mes = pd.read_excel(caminho_arquivo)
+                dfs.append(df_mes)
+
+    # Juntar tudo em um só DataFrame
+    df_all = pd.concat(dfs, ignore_index=True)
+
+    # Ordenar por cliente + data do pedido
+    df_all = df_all.sort_values(by=["Clie Cgc Cpf", "Dt. emissão"])
+
+    # Marcar primeira compra real
+    df_all["primeira_compra"] = ~df_all["Clie Cgc Cpf"].duplicated()
+    
+    df_novos = df_all[(df_all["Dt. emissão"].dt.month == mes) &
+                (df_all['primeira_compra'] == True)]
+    
+    return df_novos['Clie Cgc Cpf']
